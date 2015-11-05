fcad39942f38462d083a2cb7dbcf647978b8c042
Jonathan Hollingsworth <jonforest@gmail.com>
2015-11-06 07:07:08 +1300

    Added a product type

--- schemas/product_schema.js
+++ schemas/product_schema.js
@@ -6,7 +6,8 @@ var Schema = mongoose.Schema;
 
 var productSchema = new Schema({
     name: {type: String, required: true},
-    tag: {type:String, required:true},
+    type: {type: String, required: true, enum: ['product']},
+    tag: {type:String, required: true, enum: ['halftools']},
     rrp: {type: Number, required: true},
     currencyCode: {type: String, required: true},
     status: {type: String, enum: [ 'live', 'deleted' ], default: 'live'},
